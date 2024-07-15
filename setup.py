e8f763f3976724672d1c25b19533c07fd5cb8c79
Konstantin Ivanov <67516081+ikonsta@users.noreply.github.com>
2024-07-15 13:19:27 +0300

    Add files via upload
    Signed-off-by: Konstantin Ivanov <67516081+ikonsta@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+  
+setuptools.setup(
+  name='torch_conv_kan',
+  version='0.1.0',
+  author='Ivan Drokin',
+  author_email='',
+  description='',
+  long_description='',
+  long_description_content_type='',
+  url='https://github.com/ikonsta/torch-conv-kan',
+  packages=setuptools.find_packages()
+)
