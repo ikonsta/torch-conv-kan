d68ab0186e5715cf936f942d1784ac387a4e5f0e
Konstantin Ivanov <const.ivanoff@gmail.com>
2024-07-15 15:06:12 +0300

    Updates

--- torch_conv_kan/__init__.py
+++ torch_conv_kan/__init__.py
@@ -1,4 +1,1 @@
-from torch_conv_kan import utils
-from torch_conv_kan import kans
-from torch_conv_kan import kan_convs
-from torch_conv_kan import kan_peft 
+from torch_conv_kan import utils, kans ,kan_convs, kan_peft 
